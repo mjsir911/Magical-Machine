89b634ac39259843f9c0eaf1508a8f9ec67a760d
Marco Sirabella <msirabel@gmail.com>
2016-12-09 23:04:53 -0500

    - PEP8 compliancy

--- computer.py
+++ computer.py
@@ -14,7 +14,8 @@ __email__       = "msirabel@gmail.com"
 __status__      = "Prototype"  # "Prototype", "Development" or "Production"
 __module__      = ""
 
-class Word(int):# I just really wanted this to inherit int
+
+class Word(int):  # I just really wanted this to inherit int
 
     def __new__(cls, arch, bits=0b0000):
         return super(Word, cls).__new__(cls, bits)
@@ -22,22 +23,19 @@ class Word(int):# I just really wanted this to inherit int
     def __init__(self, arch, bits=0b0000):
         super().__init__()
         self.arch = arch
-        self._memory = (ceil(log(arch/2, 2)))
-        self._bits = bits # Our full data as an integer
+        self._memory = (ceil(log(arch / 2, 2)))
+        self._bits = bits  # Our full data as an integer
 
     def __repr__(self):
-        #return repr(self.inst + " | " + self.data)
+        # return repr(self.inst + " | " + self.data)
         return repr("{}W{}W{}".format(self.inst, self.data, self.regs))
 
-    def __str__(self):
-        #return str("{}W{}W{}".format(self.inst, self.data, self.regs))
+    def __str__(self):  # Not the best practice, but needed for below functions
+        # return str("{}W{}W{}".format(self.inst, self.data, self.regs))
         return format(self.bits, '0{}b'.format(self.arch * 2))
 
     def __bool__(self):
         return bool(self.bits)
-
-    #def __add__(self, other):
-        #return Word(self.arch, self.bits + other)
 
     @property
     def bits(self):
@@ -45,53 +43,54 @@ class Word(int):# I just really wanted this to inherit int
 
     @bits.setter
     def bits(self, bits):
-        #print(type(bits))
-        #print(bits)
+        # print(type(bits))
+        # print(bits)
         assert 0 <= bits < (2 ** self.arch) ** 2
-        assert type(bits) == type(int())
+        assert isinstance(bits, int)
         self._bits = bits
 
-    #@property
-    #def _str_bits(self):
-        #return format(self.bits, '0{}b'.format(self.arch * 2))
-
     @property
-    def inst(self):#or instruction?
+    def inst(self):  # or instruction?
         """Return first half(instruction) of data as an integer"""
-        #return int(format(self.bits, '0{}b'.format(self.arch * 2))[:self.arch], 2)
         return int(str(self)[:self.arch - self._memory], 2)
 
     @property
     def data(self):
         """Return second half(data) of data as an integer"""
-        #return int(format(self.bits, '0{}b'.format(self.arch * 2))[self.arch:], 2)
-        return int(str(self)[self.arch - self._memory : self.arch * 2 - self._memory], 2)
+        return int(str(self)[
+            self.arch     - self._memory:
+            self.arch * 2 - self._memory
+        ], 2)
 
     @property
     def regs(self):
         """Returns register of data as integer"""
         return int(str(self)[self.arch * 2 - self._memory:], 2)
 
+
 class Memory(tuple):
     """A device containing many words of data(architecture squared)"""
-    def __new__(cls, arch, amount = 0):
+    def __new__(cls, arch, amount=0):
         if not amount:
             amount = 2 ** arch
-        return super(Memory, cls).__new__(cls,
-                (Word(arch) for i in range(amount)))
+        return super(Memory, cls).__new__(
+            cls,
+            (Word(arch) for i in range(amount))
+        )
 
-    def __init__(self, arch, amount = 0):
+    def __init__(self, arch, amount=0):
         self._arch = arch
 
     def reboot(self):
         """Wipe all values in ram and reinitialize"""
-        #self = self.__new__(type(self), self._arch)
+        # self = self.__new__(type(self), self._arch)
         for word in self:
             word.magnet()
 
+
 class Chip:
     def __init__(self, name, lam):
-        #self.self = parent
+        # self.self = parent
         self.name = name
         self.function = lam
         self.subname = 'chip'
@@ -99,13 +98,14 @@ class Chip:
     def __repr__(self):
         orig = super().__repr__()
         name = type(self).__name__
-        #return '<bound operator ALU.{} of {}>'\
-                #.format(self.name.upper(), repr(self.self))
+        # return '<bound operator ALU.{} of {}>'\
+        #         .format(self.name.upper(), repr(self.self))
         return orig.replace(name, '{}.{}'.format(name, self.name.upper()).replace('object', self.subname))
 
     def __call__(self, reg, mem):
-        #reg.bits = self.function(reg.bits, mem.bits)
+        # reg.bits = self.function(reg.bits, mem.bits)
         self.function(reg.bits, mem.bits)
+
 
 class ALU(Chip):
     def __init__(self, name, operator):
@@ -115,23 +115,24 @@ class ALU(Chip):
     def __call__(self, reg, mem):
         reg.bits = int(self.function(reg.bits, mem.bits))
 
+
 class SIO(Chip):
     def __init__(self, name, bus):
         super().__init__(name, bus)
         self.subname = 'serial'
 
     def __call__(self, reg, mem):
-        #print(mem)
+        # print(mem)
         input = self.function(reg, mem)
-        #if input is not mem or input is not mem.bits:
+        # if input is not mem or input is not mem.bits:
         if input is mem.bits:
-            #print('output = reg')
+            # print('output = reg')
             output = reg
         else:
             output = mem
 
-        #print(output)
-        output.bits = input #Somehow function this works
+        # print(output)
+        output.bits = input  # Somehow function this works
 
 
 class CPU:
@@ -141,23 +142,24 @@ class CPU:
 
         self.ram = Memory(self.arch, 2 ** self.arch)
 
-        self.registers = Memory(self.arch, 2 ** (ceil(log(arch/2, 2))))
+        self.registers = Memory(self.arch, 2 ** (ceil(log(arch / 2, 2))))
 
-        self.counter = self.registers[-1] # Use last register as counter
-        self.iReg    = self.registers[-2] # Second to last register as instructional register
+        self.counter = self.registers[-1]  # Use last register as counter
+        self.iReg    = self.registers[-2]  # Second to last register as instructional register
         self.iSet = (
-                Chip('nop', lambda r, m: 0),
-                SIO('load', lambda r, m: m.bits),
-                SIO('stor', lambda r, m: r.bits),
-                ALU('add',  lambda r, m: r + m),
-                ALU('sub',  lambda r, m: r - m),
-                ALU('mul',  lambda r, m: r * m),
-                ALU('div',  lambda r, m: r / m),
-                Chip('nop', lambda r, m: 0,),
-                            # Cuz 4-bit calc skips this for SOME reason
-                Chip('prn', lambda r, m: print(m)),
-                SIO('inpt', lambda r, m: int(input(), 0)),
-                )
+            Chip('nop', lambda r, m: 0),
+            SIO('load', lambda r, m: m.bits),
+            SIO('stor', lambda r, m: r.bits),
+            # Mathematical
+            ALU('add', lambda r, m: r + m),
+            ALU('sub', lambda r, m: r - m),
+            ALU('mul', lambda r, m: r * m),
+            ALU('div', lambda r, m: r / m),
+            Chip'nop', lambda r, m: 0,),
+            # Cuz 4-bit calc skips this for SOME reason
+            Chip('prn', lambda r, m: print(m)),
+            SIO('inpt', lambda r, m: int(input(), 0)),
+        )
 
     def fetch(self):
         self.iReg.bits = self.ram[self.counter.bits].bits
